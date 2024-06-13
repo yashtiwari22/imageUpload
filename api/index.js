e64fd8bd138844824c04bec8de545ff5754b4134
Yash Tiwari <85613117+yashtiwari22@users.noreply.github.com>
2024-06-13 16:32:31 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', require('./routes/app.route'));
 
-const server = app.listen(4000,function(){
-    console.log('Server Ready');
+const server = app.listen(4000,(req,res){
+    res.status(200).json("Hello uploader");
 });
 server.setTimeout(300000);
